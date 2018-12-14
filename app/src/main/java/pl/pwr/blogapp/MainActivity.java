10b86bbfff6c4ca526d57555442b45871fd6e722
PawelZelezik <pawel.zelezik@hotmail.com>
2018-12-14 14:25:36 +0100

    New Page ( Reg Page).

--- app/src/main/java/pl/pwr/blogapp/MainActivity.java
+++ app/src/main/java/pl/pwr/blogapp/MainActivity.java
@@ -6,19 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mainToolbar;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        mAuth = FirebaseAuth.getInstance();
+
+        mainToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
         getSupportActionBar().setTitle("DailyBlogDuck");
     }
@@ -29,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if(currentUser == null){
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(loginIntent);
-            finish();
+            sendToLogin();
         }
     }
 
@@ -42,6 +44,37 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_logout_btn:
+                logOut();
+
+                return true;
+
+                default:
+
+                    return false;
+
+        }
+    }
+
+    private void logOut() {
+
+        mAuth.signOut();
+        sendToLogin();
+    }
+
+
+    private void sendToLogin() {
+
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
 
     }
 }
