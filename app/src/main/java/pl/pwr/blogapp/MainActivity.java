9c282bb53aaaa91bbeba08de3e1be86b22a75ee6
PawelZelezik <pawel.zelezik@hotmail.com>
2019-01-14 22:58:39 +0100

    Main Page future.

--- app/src/main/java/pl/pwr/blogapp/MainActivity.java
+++ app/src/main/java/pl/pwr/blogapp/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.icu.text.UnicodeSetSpanner;
 import android.sax.StartElementListener;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -33,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton addPostBtn;
 
+    private BottomNavigationView mainbottomNav;
+
+    private HomeFragment homeFragment;
+    private NotificationFragment notificationFragment;
+    private AccountFragment accountFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +54,42 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mainToolbar);
         addPostBtn = findViewById(R.id.add_post_btn);
         getSupportActionBar().setTitle("DailyBlogDuck");
+
+        mainbottomNav = findViewById(R.id.main_bottom_nav);
+
+        //Fragments
+
+        homeFragment = new HomeFragment();
+        notificationFragment = new NotificationFragment();
+        accountFragment = new AccountFragment();
+
+        //End of fragments
+
+        mainbottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+
+                    case R.id.bottom_action_home :
+                        replaceFragment(homeFragment);
+                        return true;
+
+                    case R.id.bottom_action_notification :
+                        replaceFragment(notificationFragment);
+                        return true;
+
+                    case R.id.bottom_action_account :
+                        replaceFragment(accountFragment);
+                        return true;
+
+                        default :
+                            return false;
+
+                }
+            }
+        });
+
 
         addPostBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -139,6 +184,14 @@ public class MainActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
         finish();
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragment);
+        fragmentTransaction.commit();
 
     }
 }
--- app/src/main/java/pl/pwr/blogapp/NewPostActivity.java
+++ app/src/main/java/pl/pwr/blogapp/NewPostActivity.java
@@ -2,6 +2,8 @@ package pl.pwr.blogapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapRegionDecoder;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,6 +19,8 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -28,10 +32,19 @@ import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import id.zelory.compressor.Compressor;
 
 public class NewPostActivity extends AppCompatActivity {
+
 
     private Toolbar newPostToolbar;
     private ImageView newPostImage;
@@ -45,6 +58,8 @@ public class NewPostActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private String current_user_id;
+
+    private Bitmap compressedImageFile;
 
 
 
@@ -89,43 +104,81 @@ public class NewPostActivity extends AppCompatActivity {
 
                     newPostProgres.setVisibility(View.VISIBLE);
 
-                    String randomName = FieldValue.serverTimestamp().toString();
+                    final String randomName = UUID.randomUUID().toString();
+
                     StorageReference filePath = storageReference.child("post_images").child(randomName + ".jpg");
                     filePath.putFile(postImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
 
+                            Task<Uri> uri_image = task.getResult().getStorage().getDownloadUrl();
+                            while (!uri_image.isComplete()) ;
+                            final String download_uri = uri_image.getResult().toString();
+
                             if(task.isSuccessful()){
 
-                                Task<Uri> uri_image = task.getResult().getStorage().getDownloadUrl();
-                                while (!uri_image.isComplete()) ;
-                                String download_uri = uri_image.getResult().toString();
+                                File newImageFile = new File(postImageUri.getPath());
 
-                                Map<String, Object> postMap = new HashMap<>();
-                                postMap.put("imageUrl", download_uri);
-                                postMap.put("desc", desc);
-                                postMap.put("user_id", current_user_id);
-                                postMap.put("timestamp", FieldValue.serverTimestamp());
+                                try {
+                                    compressedImageFile = new Compressor(NewPostActivity.this)
+                                            .setMaxHeight(200)
+                                            .setMaxWidth(360)
+                                            .setQuality(25)
+                                            .compressToBitmap(newImageFile);
 
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
 
-                                firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                                byte[] thumbData = baos.toByteArray();
+
+                                UploadTask uploadTask = storageReference.child("post_images/thumbs")
+                                        .child(randomName + ".jpg").putBytes(thumbData);
+
+                                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                                     @Override
-                                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        Task<Uri> task = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                                        String downloadThumbUri = task.toString();
 
-                                        if(task.isSuccessful()){
+                                        Map<String, Object> postMap = new HashMap<>();
+                                        postMap.put("imageUrl", download_uri);
+                                        postMap.put("thumbUrl", downloadThumbUri);
+                                        postMap.put("desc", desc);
+                                        postMap.put("user_id", current_user_id);
+                                        postMap.put("timestamp", FieldValue.serverTimestamp());
 
-                                            Toast.makeText(NewPostActivity.this, "Post was added", Toast.LENGTH_LONG).show();
-                                            finish();
 
-                                        }else{
+                                        firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentReference> task) {
 
-                                        }
+                                                if(task.isSuccessful()){
 
-                                        newPostProgres.setVisibility(View.INVISIBLE);
+                                                    Toast.makeText(NewPostActivity.this, "Post was added", Toast.LENGTH_LONG).show();
+                                                    finish();
+
+                                                }else{
+
+                                                }
+
+                                                newPostProgres.setVisibility(View.INVISIBLE);
+
+                                            }
+                                        });
+
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                        //Error info
 
                                     }
                                 });
-
 
                             }else{
 
@@ -171,5 +224,4 @@ public class NewPostActivity extends AppCompatActivity {
             }
         }
     }
-
 }
